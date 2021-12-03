be4c9e99086649fb714b3c78ebd7d64fc0f7d608
danielinsuasti <daniel.insuasti@correounivalle.edu.co>
2021-12-03 18:38:44 -0500

    Correccion del puerto del Back

--- server.js
+++ server.js
@@ -18,6 +18,8 @@ dotenv.config({ path: './.env' });
 
 const app = Express();
 
+const port = process.env.PORT || 5000;
+
 app.use(Express.json());
 app.use(Cors());
 
@@ -43,8 +45,8 @@ app.use(rutasUsuario);
 app.use(rutasVenta);
 
 const main = () => {
-  return app.listen(process.env.PORT, () => {
-    console.log(`escuchando puerto ${process.env.PORT}`);
+  return app.listen(port, () => {
+    console.log(`escuchando puerto ${port}`);
   });
 };
 
